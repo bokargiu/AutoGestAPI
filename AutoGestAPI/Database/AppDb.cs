2ec8ba2c30638ddf6584a7fd2df48cdc89775470
bokargiu <erickjstaack07@gmail.com>
2026-01-19 20:47:06 -0300

    DB configured

---
+++ AutoGestAPI/Database/AppDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutoGestAPI.Database
+{
+    public class AppDb : DbContext
+    {
+        public AppDb(DbContextOptions<AppDb> options) : base(options) { }
+    }
+}
--- AutoGestAPI/Program.cs
+++ AutoGestAPI/Program.cs
@@ -1,3 +1,7 @@
+using AutoGestAPI.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Cors Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowSites", policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
+});
+#endregion
+
+#region DB Configuration
+var connection = builder.Configuration.GetConnectionString("Connection");
+builder.Services.AddDbContext<AppDb>(options =>
+{
+    options.UseMySql(connection, ServerVersion.AutoDetect(connection));
+});
+#endregion
+
 
 var app = builder.Build();
 
