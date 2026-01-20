67ae375f5be9a21dc37b2cd6f12c6818c8e55c09
bokargiu <erickjstaack07@gmail.com>
2026-01-19 21:03:47 -0300

    adicionei o User Model

---
+++ AutoGestAPI/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using BCrypt.Net;
+
+namespace AutoGestAPI.Models
+{
+    public class User
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public User() { }
+
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = BCrypt.Net.BCrypt.HashPassword(password);
+        }
+    }
+}
