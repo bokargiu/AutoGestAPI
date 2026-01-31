eb1008efe740c0775c90273218da29c2d88dec9c
bokargiu <erickjstaack07@gmail.com>
2026-01-31 19:45:01 -0300

    OrderDto para criar uma Order foi criada

---
+++ AutoGestAPI/DTO's/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using AutoGestAPI.Models;
+
+namespace AutoGestAPI.DTO_s
+{
+    public class OrderDto
+    {
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public Guid ClientId { get; set; }
+
+    }
+}
