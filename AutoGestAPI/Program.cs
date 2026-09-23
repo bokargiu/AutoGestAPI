f3b87793a4d5a1422434fb93621472910a227031
bokargiu <erickjstaack07@gmail.com>
2026-09-23 09:45:12 -0300

    Algumas alterações nas configs do docker para rodar em DEV e em PROD de formas diferentes

--- AutoGestAPI/Program.cs
+++ AutoGestAPI/Program.cs
@@ -8,10 +8,20 @@ using AutoGestAPI.Services.OrderServices;
 using AutoGestAPI.Services.ServiceServices;
 using AutoGestAPI.Services.SingUpServices;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.Net;
 using System.Runtime.CompilerServices;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.RateLimiting;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,9 +42,9 @@ builder.Services.AddCors(options =>
 #endregion
 
 #region DB Configuration
-var connection = builder.Configuration.GetConnectionString(builder.Environment.IsDevelopment() 
-                                                            ? "LocalConnection" 
-                                                            : "DockerConnection");
+var connection = builder.Environment.IsDevelopment() 
+                    ? builder.Configuration["ConnectionStrings:Connection"]
+                    : File.ReadAllText("/run/secrets/autogest-connection").Trim();
 builder.Services.AddDbContext<AppDb>(options =>
 {
     options.UseMySql(connection, ServerVersion.AutoDetect(connection));
@@ -75,6 +85,61 @@ builder.Services.AddAuthentication(options =>
 });
 #endregion
 
+#region Rate Limit
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto;
+
+    //options.KnownProxies.Add(
+    //    IPAddress.Parse("127.0.0.1")
+    //);
+    options.KnownProxies.Add(
+        IPAddress.Parse("187.75.67.81")
+    );
+});
+
+builder.Services.AddRateLimiter(options =>
+{
+    //options.AddTokenBucketLimiter("BruteForceProtection", opt =>
+    //{
+    //    opt.TokenLimit = 10;
+    //    opt.ReplenishmentPeriod = TimeSpan.FromSeconds(30);
+    //    opt.TokensPerPeriod = 5;
+    //    opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    //    opt.QueueLimit = 0;
+    //});
+
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(
+        httpContext =>
+        {
+            var user = httpContext.User.Identity;
+            if (user != null && user.IsAuthenticated)
+                return RateLimitPartition.GetFixedWindowLimiter(
+                    partitionKey: httpContext.User.FindFirst(ClaimTypes.PrimarySid)?.Value ?? "anonymous",
+                    factory: _ => new FixedWindowRateLimiterOptions
+                    {
+                        PermitLimit = 10,
+                        QueueLimit = 0,
+                        Window = TimeSpan.FromSeconds(30)
+                    });
+
+            return RateLimitPartition.GetTokenBucketLimiter(
+                partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+                factory: _ => new TokenBucketRateLimiterOptions
+                {
+                    TokenLimit = 10,
+                    TokensPerPeriod = 5,
+                    ReplenishmentPeriod = TimeSpan.FromMinutes(1),
+                    QueueLimit = 0
+                });
+        });
+
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+});
+#endregion
+
 #region Add Services
 builder.Services.AddScoped<AppDb>();
 builder.Services.AddScoped<User>();
@@ -94,7 +159,48 @@ builder.Services.AddScoped<IOrderService,  OrderService>();
 
 var app = builder.Build();
 
+
+app.UseForwardedHeaders();
+
+app.UseCors("AllowSites");
+
+app.UseAuthentication();
+
+app.UseRateLimiter();
+
+app.UseAuthorization();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
 app.MapGet("/", () => "Api está funcionando!");
+
+app.MapGet("/ping", () => StatusCodes.Status200OK);
+
+app.MapGet("/db", async (AppDb db, HttpContext _) =>
+{
+    var inicio = Stopwatch.StartNew();
+
+    await db.Database.OpenConnectionAsync();
+
+    var abertura = inicio.Elapsed;
+
+    var result = await db.Service
+        .AsNoTracking()
+        .Take(1)
+        .ToListAsync();
+
+    var query = inicio.Elapsed;
+
+    await db.Database.CloseConnectionAsync();
+
+    var total = inicio.Elapsed;
+
+    Console.WriteLine(
+        $"Conexão: {abertura.TotalMilliseconds:F2} ms | " +
+        $"Query: {(query - abertura).TotalMilliseconds:F2} ms | " +
+        $"Total: {total.TotalMilliseconds:F2} ms | "
+    );
+});
 
 using (var scope = app.Services.CreateScope()) { // Adicionando Migrações
     var db = scope.ServiceProvider.GetRequiredService<AppDb>();
@@ -108,13 +214,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseMiddleware<GlobalExceptionMiddleware>();
-
-app.UseCors("AllowSites");
-
 //app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
